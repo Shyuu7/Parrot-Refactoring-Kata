fe15f78dfbc8e2b315817732a86691a3c0b169b4
Larissa Conti <laricbarros@gmail.com>
2025-11-09 12:34:01 -0300

    passado atributo numberOfCoconuts para dentro da subclasse AfricanParrot, pois é a única que o utiliza

--- Java/src/main/java/parrot/NorwegianParrot.java
+++ Java/src/main/java/parrot/NorwegianParrot.java
@@ -2,7 +2,7 @@ package parrot;
 
 public class NorwegianParrot extends Parrot {
     protected NorwegianParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(numberOfCoconuts, voltage, isNailed);
+        super(voltage, isNailed);
     }
 
     @Override
